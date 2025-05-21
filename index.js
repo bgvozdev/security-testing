61b5c3614db2cd67839ad88ff6d5019642228422
bgvozdev <bgvozdev@atlassian.com>
2025-05-21 13:29:25 +1000

    TEST-10: Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 password = "my-super-secure-password"
 tokenProd = "f0962fef-324f-4957-b5f1-a08ac035923a";
+tokenStg = "f0962fef-324f-4957-b5f1-a08ac035923a";
 
 console.log(password, "it must not be logged");
 
